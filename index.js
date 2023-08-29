115bf72e3c5540e38d0e5bb0f24ce140fdecac86
nirglus <nirgluskin@gmail.com>
2023-08-29 15:47:54 +0300

    set img1+2 vars

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 let randomNum1 = Math.floor(Math.random()*6 + 1);
 let randomNum2 = Math.floor(Math.random()*6 + 1);
+
+const img1 = document.querySelector(".img1");
+const img2 = document.querySelector(".img2");
+
