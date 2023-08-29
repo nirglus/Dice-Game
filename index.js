f50a72fc09424fc0929d49751a98ff941848b5ae
nirglus <nirgluskin@gmail.com>
2023-08-29 15:54:52 +0300

    add if else statements

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const img2 = document.querySelector(".img2");
 
 img1.setAttribute("src", "images/dice" + randomNum1 + ".png");
 img2.setAttribute("src", "images/dice" + randomNum2 + ".png");
+
+const heading = document.querySelector("h1");
+if(randomNum1 > randomNum2){
+    heading.innerHTML = "Player 1 has won!"
+}else if(randomNum1 < randomNum2){
+    heading.innerHTML = "Player 2 has won!"
+}else{
+    heading.innerHTML = "It's a draw!"
+}
