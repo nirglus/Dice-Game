13a8dedd589c520514b44688b1fb0ef53398f325
nirglus <nirgluskin@gmail.com>
2023-08-29 15:49:44 +0300

    set atr for both img1 & img2

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ let randomNum2 = Math.floor(Math.random()*6 + 1);
 const img1 = document.querySelector(".img1");
 const img2 = document.querySelector(".img2");
 
+img1.setAttribute("src", "images/dice" + randomNum1 + ".png");
+img2.setAttribute("src", "images/dice" + randomNum2 + ".png");
